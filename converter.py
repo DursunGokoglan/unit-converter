acf3622999401a645a9b82111516589558da7d7e
Dursun <dursungokoglan@gmail.com>
2023-11-26 16:12:25 +0300

    Switch functionality added

--- converter.py
+++ converter.py
@@ -4,8 +4,8 @@ from tkinter import *
 class Converter:
     def __init__(self, converting_from, converting_to, multiplier, **kwargs):
         self.entry = Entry()
-        self.entry.insert(END, "0")
-        self.button = Button(text=kwargs.get('button', "Calculate"), command=self.convert)
+        self.button_c = Button(text=kwargs.get('button', "Calculate"), command=self.convert)
+        self.button_s = Button(text=kwargs.get('button', "Switch"), command=self.switch)
         self.input_unit = Label(text=converting_from)
         self.output_unit = Label(text=converting_to)
         self.result_label = Label()
@@ -13,11 +13,25 @@ class Converter:
         self.multiplier = multiplier
 
         self.entry.grid(column=1, row=0)
-        self.button.grid(column=1, row=2)
+        self.button_c.grid(column=1, row=2)
+        self.button_s.grid(column=2, row=2)
         self.input_unit.grid(column=2, row=0)
         self.output_unit.grid(column=2, row=1)
         self.result_label.grid(column=1, row=1)
         self.presenter.grid(column=0, row=1)
 
     def convert(self):
-        self.result_label.config(text=float(self.entry.get()) * self.multiplier)
+        self.result_label.config(text=round(float(self.entry.get()) * self.multiplier, 4))
+
+    def switch(self):
+        if self.input_unit.grid_info()["row"] == 0:
+            self.input_unit.grid(column=2, row=1)
+            self.output_unit.grid(column=2, row=0)
+
+        else:
+            self.input_unit.grid(column=2, row=0)
+            self.output_unit.grid(column=2, row=1)
+
+        self.entry.delete(0, END)
+        self.result_label.config(text="")
+        self.multiplier = 1 / self.multiplier
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from converter import Converter
 
+input_unit = input("Enter input unit: ")
+output_unit = input("Enter output unit: ")
+ratio = float(input("Enter input/output ratio: "))
+
 window = Tk()
 window.title("Convert")
 window.config(padx=10, pady=10)
 
-converter = Converter("Km", "Mile", 0.621371)
-converter.convert()
+converter = Converter(input_unit, output_unit, ratio)
 
 window.mainloop()
